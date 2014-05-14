a1fa1f0768c1a66115d3cd23f3b55f8a9ab2a9a4
Sam Barnes <sam@avantrural.com>
2014-05-14 10:12:00 +0100

    much improved

--- Readme.md
+++ Readme.md
@@ -1,4 +1,24 @@
 HTML5 drag and drop for AngularJS
 ---------------------------------
 
-Check out the example or read the source...
+Data driven:
+
+    <div sb-draggable sb-dragdata="{{obj.id}}">{{obj.title}}</div>
+
+Drop targets:
+
+    <div sb-drop-target sb-drop-effect="copy"></div>
+
+Set drop effect and take care of the awkward preventDefault() calls on dragover & dragenter events necessary to enable dropping.
+
+Event handlers injectable with:
+
+ - `$event` native drag event  
+ - `$data`, data defined in sb-dragdata
+ - `$element` the element being dragged
+ - `$completed` boolean specifying whether drag has been handled by a drop zone.
+
+
+    <div sb-draggable sb-dragend="onDragend($completed)"></div>
+    <div sb-drop-target sb-drop="onDrop($data)"></div>
+
--- sb-drag-and-drop.js
+++ sb-drag-and-drop.js
@@ -1,12 +1,17 @@
 
 angular.module('sbDragAndDrop', [])
 
+  .config(function () {
+    if (window.jQuery) jQuery.event.props.push('dataTransfer'); 
+  })
+
   .provider('sbDragController', [function () {
 
     var Ctrl = function () {
       this._counter = 0;
       this._elements = {};
       this._data = {};
+      this._completed = {};
     };
 
     Ctrl.prototype.dragStart = function (event, element, data) {
@@ -17,19 +22,7 @@ angular.module('sbDragAndDrop', [])
     };
 
     Ctrl.prototype.getId = function (event) {
-      return event.dataTransfer.getData('sbDragId');
-    };
-
-    Ctrl.prototype.getData = function (event) {
-      return this._data[this.getId(event)];
-    };
-
-    Ctrl.prototype.getElement = function (event) {
-      return this._elements[this.getId(event)];
-    };
-
-    Ctrl.prototype.dragEnd = function (event) {
-      var id = this.getId(event);
+      var id =  event.dataTransfer.getData('sbDragId');
 
       // Work around some browsers not allowing getData on dragend
       if (!id) {
@@ -37,10 +30,30 @@ angular.module('sbDragAndDrop', [])
           if (this._elements[id] === event.target) break;
         }
       }
+      return id;
+    };
+
+    Ctrl.prototype.getContext = function (event) {
+      var id = this.getId(event);
+      return {
+        $event: event, 
+        $data: this._data[id],
+        $element: this._elements[id],
+        $completed: this._completed[id]
+      };
+    };
+
+    Ctrl.prototype.setCompleted = function (event) {
+      this._completed[this.getId(event)] = true;
+    };
+
+    Ctrl.prototype.dragEnd = function (event) {
+      var id = this.getId(event);
 
       if (id) {
         delete this._data[id];
         delete this._elements[id];
+        delete this._completed[id];
       }
     };
 
@@ -51,85 +64,101 @@ angular.module('sbDragAndDrop', [])
 
   .directive('sbDraggable', ['$parse', 'sbDragController', function($parse, ctrl) {
 
-      return {
-        restrict: 'A',
-        link: function(scope, elem, attrs) {
-          window.ctrl = ctrl;
-          attrs.$set('draggable', true);
+    return {
+      restrict: 'A',
+      link: function(scope, elem, attrs) {
+        attrs.$set('draggable', true);
 
-          elem.bind('dragstart', function(event) {
-            attrs.$addClass('sb-dragging');
+        var handlers = {};
 
-            ctrl.dragStart(event, elem, scope.$eval(attrs.sbDragdata));
+        ['Dragstart', 'Drag', 'Dragend'].forEach(function (name) {
+          name = 'sb'+name;
+          if (name in attrs) {
+            var fn = $parse(attrs[name]);
+            handlers[name] = function (event) {
+              return scope.$apply(function () {
+                return fn(scope, ctrl.getContext(event));
+              });
+            };
+          }
+          else handlers[name] = angular.noop;
+        });
 
-            if ('sbDragstart' in attrs) {
-              var fn = $parse(attrs.sbDragstart);
-              // Dont trigger digest here - rely on click event
-              fn(scope, {$event:event, $element: elem});
-            }
-          });
-
-          elem.bind('dragend', function(event) {
-            attrs.$removeClass('sb-dragging');
-            ctrl.dragEnd(event);
-          });
+        function callHandler(name, event) {
+          if (name in handlers) handlers[name](event);
         }
-      };
-    }])
+
+        elem.bind('dragstart', function(event) {
+          ctrl.dragStart(event, elem, scope.$eval(attrs.sbDragdata));
+          var res = handlers.sbDragstart(event);
+          if (res === false) {
+            ctrl.dragEnd(event);
+            return false;
+          }
+          attrs.$addClass('sb-dragging');
+        });
+
+        elem.bind('dragend', function(event) {
+          handlers.sbDragend(event);
+          ctrl.dragEnd(event);
+          attrs.$removeClass('sb-dragging');
+        });
+      }
+    };
+  }])
 
   .directive('sbDropTarget', ['$parse', 'sbDragController', function($parse, ctrl) {
-
-    // To do: handle dragenter event?
 
     return {
       restrict: 'A',
       link: function(scope, elem, attrs, controller) {
         var dropEffect = attrs.sbDropEffect || 'move';
-        var onDragOver = function (e) {};
-        var onDrop = function (e) {};
+        var handlers = {};
 
-        if ('sbDragover' in attrs) {
-          var overFn = $parse(attrs.sbDragover);
-          onDragOver = function (e) {
-            return scope.$apply(function () {
-              return overFn(scope, {$event:e, $data: ctrl.getData(e)});
-            });
-          };
-        }
-
-
-        if ('sbDrop' in attrs) {
-          var dropFn = $parse(attrs.sbDrop);
-          onDrop = function (e) {
-            scope.$apply(function () {
-              dropFn(scope, {$event:e, $data: ctrl.getData(e)});
-            });
-          };
-        }
-
-        elem.bind('dragover', function(e) {
-          var res = onDragOver(e);
-
-          if (res !== false) {
-            // Prevent default tells browser drop is permitted
-            e.preventDefault();
-            e.dataTransfer.dropEffect = dropEffect;
-            attrs.$addClass('sb-dragover');
+        ['Dragenter', 'Dragover', 'Dragleave', 'Drop'].forEach(function (name) {
+          name = 'sb'+name;
+          if (name in attrs) {
+            var fn = $parse(attrs[name]);
+            handlers[name] = function (event) {
+              return scope.$apply(function () {
+                return fn(scope, ctrl.getContext(event));
+              });
+            };
           }
+          else handlers[name] = angular.noop;
         });
 
-        elem.bind('dragleave', function(e) {
+        // With HTML's drag and drop event handling calling preventDefault enables 
+        // the element as a drop zone. The opposite behaviour (returning false to disable 
+        // the dropzone) Seems more appropriate here...
+
+        elem.bind('dragenter', function(event) {
+          var res = handlers.sbDragenter(event);
+          if (res === false) return;
+          event.preventDefault();
+          event.dataTransfer.dropEffect = dropEffect;
+          attrs.$addClass('sb-dragover');
+        });
+
+        elem.bind('dragover', function(event) {
+          var res = handlers.sbDragover(event);
+          if (res === false) return;
+          event.preventDefault();
+          event.dataTransfer.dropEffect = dropEffect;
+        });
+
+        elem.bind('dragleave', function(event) {
+          handlers.sbDragleave(event);
           attrs.$removeClass('sb-dragover');
         });
 
-        elem.bind('drop', function(e) {
+        elem.bind('drop', function(event) {
+          // calling preventDefault stops the browser's native drop handling
+          if (event.preventDefault) event.preventDefault();
+          if (event.stopPropagation) event.stopPropagation();
+          var res = handlers.sbDrop(event);
+          if (res !== false) ctrl.setCompleted(event);
           attrs.$removeClass('sb-dragover');
-
-          // Prevent default browser drop handling
-          if (e.preventDefault) e.preventDefault();
-          if (e.stopPropagation) e.stopPropagation();
-
-          onDrop(e);
         });
       }
     };
